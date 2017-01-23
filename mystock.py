1dacbd4591cb2c7f76021a5bdd55c7462984a367
douyongtao <douyongtao@wisdomgarden.com>
2017-01-23 16:46:47 +0800

    add gunicorn support

--- mystock.py
+++ mystock.py
@@ -1,11 +1,6 @@
 import os
 
-from invoke import ctask
-
-
-@ctask
-def server(ctx):
+if __name__ == '__main__':
     from app import create_app
     app = create_app(os.getenv('FLASK_CONFIG') or 'default')
     app.run(debug=True)
-
