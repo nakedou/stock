cbb65f0c267bbc3948253639eebfbd15c76502c4
douyongtao <douyongtao@wisdomgarden.com>
2017-04-15 02:03:40 +0800

    fix invoke dependence

--- update_dependences.sh
+++ update_dependences.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-cd $(dirname $0)/..
-
-
 pip install -r requirements.txt
 
 npm install --python=python2.7
-node_modules/bower/bin/bower install
+node_modules/bower/bin/bower install --allow-root
 
 if [ "$(uname -s)" == "Darwin" ] && ! brew list -1 | grep -q pngquant; then
     echo 'Install pngquant on Mac...'
